8c66ceae3e4d26049e56aea5c64aa1eba9f7afe8
Tan Tze Hong <thtan@greatech-group.com>
2025-04-16 10:54:28 +0800

    Add method 2 , question 26

---
+++ LeetCodeDemo/169_MajorityElement.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeDemo
+{
+    public class _169MajorityElement
+    {
+
+        static void Main(string[] args)
+        {
+
+
+        }
+
+        public int MajorityElement(int[] nums)
+        {
+            return 0;
+        }
+    }
+}
+
--- LeetCodeDemo/26_RemoveDuplicatesfromSortedArray.cs
+++ LeetCodeDemo/26_RemoveDuplicatesfromSortedArray.cs
@@ -9,55 +9,76 @@ namespace LeetCodeDemo
 {
     public  class _26_RemoveDuplicatesfromSortedArray
     {
-        static void Main(string[] args) {
-            int[] num1 = { 1, 0, 2 };
-            int[] num2 = { 0, 0, 1, 1, 2, 2, 3, 4 };
-            int[] num3 = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-            int[] num4 = { 1, 1, 2, 2, 3, 4, 5, 6, 7, 8, 9 };
+        //static void Main(string[] args)
+        //{
+        //    int[] num1 = { 1, 0, 2 };
+        //    int[] num2 = { 0, 0, 1, 1, 2, 2, 3, 4 };
+        //    int[] num3 = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        //    int[] num4 = { 1, 1, 2, 2, 3, 4, 5, 6, 7, 8, 9 };
 
-            Console.WriteLine("Unique Count: " + RemoveDuplicates(num2));
-            Console.WriteLine(string.Join(", ", num2));
+        //    Console.WriteLine("Unique Count: " + RemoveDuplicates(num1));
+        //    Console.WriteLine(string.Join(", ", num1));
 
-        }
+        //}
 
         #region Method 1
+        //public static int RemoveDuplicates(int[] nums)
+        //{
+        //    int num = 0;
+        //    int[] tempArray = new int[nums.Length];
+        //    bool zeroValue = false;
+
+        //    for (int i = 0; i < nums.Length; i++)
+        //    {
+
+        //        for (int j = 0; j < tempArray.Length; j++)
+        //        {
+
+        //            if (nums[i] == 0 && tempArray[j] == 0 && zeroValue==false) {
+        //                zeroValue = true;
+        //                tempArray[num] = nums[i];
+        //                nums[num] = tempArray[num];
+        //                num++;
+        //                break;
+        //            }
+        //            if (nums[i] != tempArray[j])
+        //            {
+        //                if (j == tempArray.Length-1)
+        //                {
+        //                    tempArray[num] = nums[i];
+        //                    nums[num] = tempArray[num];
+        //                    num++;
+        //                }
+        //            }
+        //            else {
+        //                break;
+        //            }
+        //        }
+        //    }
+
+        //    return num;
+        //}
+
+        #endregion
+
+        #region method 2
         public static int RemoveDuplicates(int[] nums)
         {
-            int num = 0;
-            int[] tempArray = new int[nums.Length];
-            bool zeroValue = false;
+            if (nums.Length == 0) return 0;
 
-            for (int i = 0; i < nums.Length; i++)
+            int index = 1;
+
+            for (int i = 1; i < nums.Length; i++)
             {
-
-                for (int j = 0; j < tempArray.Length; j++)
+                if (nums[i] != nums[i - 1])
                 {
-                    
-                    if (nums[i] == 0 && tempArray[j] == 0 && zeroValue==false) {
-                        zeroValue = true;
-                        tempArray[num] = nums[i];
-                        nums[num] = tempArray[num];
-                        num++;
-                        break;
-                    }
-                    if (nums[i] != tempArray[j])
-                    {
-                        if (j == tempArray.Length-1)
-                        {
-                            tempArray[num] = nums[i];
-                            nums[num] = tempArray[num];
-                            num++;
-                        }
-                    }
-                    else {
-                        break;
-                    }
+                    nums[index] = nums[i];
+                    index++;
                 }
             }
 
-            return num;
+            return index;
         }
-
         #endregion
 
 
