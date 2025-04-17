5c79b7606bc3a6b7387a7e92fa62212c3394c764
Tan Tze Hong <thtan@greatech-group.com>
2025-04-17 10:03:27 +0800

    121. Best Time to Buy and Sell Stock

--- LeetCodeDemo/121_BestTimetoBuyandSellStock.cs
+++ LeetCodeDemo/121_BestTimetoBuyandSellStock.cs
@@ -6,7 +6,51 @@ using System.Threading.Tasks;
 
 namespace LeetCodeDemo
 {
-    internal class _121_BestTimetoBuyandSellStock
+    public class _121_BestTimetoBuyandSellStock
     {
+        //static void Main(string[] args)
+        //{
+        //   int[] prices1 = { 7, 1, 5, 3, 6, 4 };
+        //    int[] prices2 ={ 7, 6, 4, 3, 1 };
+        //    int maxProfit = MaxProfit(prices1);
+        //    Console.WriteLine("Max Profit: " + maxProfit);
+
+
+        //}
+
+        #region Method1
+        public static int MaxProfit(int[] prices)
+        {
+            int buyPriced = prices[0];
+            int sellPrice = 0;
+            int maxProfit = 0;
+
+
+            for (int i = 0; i < prices.Length; i++)
+            {
+                if (buyPriced > prices[i])
+                {
+                    buyPriced = prices[i];
+                }
+
+                if (((prices[i] - buyPriced) > 0) && (prices[i] - buyPriced) > maxProfit)
+                {
+                    sellPrice = prices[i];
+                    maxProfit = sellPrice - buyPriced;
+                }
+
+            }
+
+            //return value
+            if (maxProfit <= 0)
+            {
+                return 0;
+            }
+            else { return maxProfit; }
+        
+            
+        }
+
+        #endregion
     }
 }
