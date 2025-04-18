87a0d195a06c5e506b51c430e686a13216e9e9d7
Tan Tze Hong <thtan@greatech-group.com>
2025-04-18 10:16:59 +0800

    125 Valid Palindrome 392 Is Subsequence

---
+++ LeetCodeDemo/125_Valid Palindrome.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeDemo
+{
+    internal class _125_Valid_Palindrome
+    {
+        //static void Main(string[] args)
+        //{
+        //    string Sentences1 = "A man, a plan, a canal: Panama";
+        //    string Sentences2 = "race a car";
+        //    string Sentences3 = " ";
+
+
+        //    Console.WriteLine("Is Palindrome: " + IsPalindrome(Sentences2));
+
+
+        //}
+
+        #region Method1
+        public static bool IsPalindrome(string s)
+        {
+            // Remove non-alphanumeric characters and convert to lowercase
+            s = new string(s.Where(c => char.IsLetterOrDigit(c)).ToArray()).ToLower();
+            int left = 0;
+            int right = s.Length - 1;
+
+            // Check for palindrome
+            while (left < right)
+            {
+                if (s[left] != s[right])
+                {
+                    return false;
+                }
+                left++;
+                right--;
+            }
+
+            return true;
+        }
+        #endregion
+
+
+    }
+}
---
+++ LeetCodeDemo/392_IsSubsequence.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeDemo
+{
+    public class _392_IsSubsequence
+    {
+        //static void Main(string[] args)
+        //{
+        //    String s = "abc";
+        //    string t = "ahbgdc";
+
+        //    Console.WriteLine(IsSubsequence(s, t));
+
+
+        //}
+
+        #region Method1
+        public static bool IsSubsequence(string s, string t)
+        {
+            //if s is empty
+            if (s.Length == 0){return true;}
+            //if t is empty & s is not empty
+            if (t.Length == 0) { return false; }
+
+
+            int sIndex = 0;
+            int tIndex = 0;
+            while (sIndex < s.Length && tIndex < t.Length)
+            {
+                if (s[sIndex] == t[tIndex])
+                {
+                    sIndex++;
+                }
+                tIndex++;
+            }
+            // If we have reached the end of s, it means all characters were found in t
+            // in the correct order.
+            if (sIndex == s.Length)
+            {
+                return true;
+            }
+            // If we have not reached the end of s, it means not all characters were found in t
+            // in the correct order.
+            else
+            {
+                return false;
+            }
+        }
+
+        #endregion
+    }
+}
